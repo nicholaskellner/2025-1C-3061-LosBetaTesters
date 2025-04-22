e53c6d3074ee913bf58aff3f0077d1a62b1adfc9
unknown <jgros2000@gmail.com>
2025-04-21 22:52:37 -0300

    Cree la clase tanque y le puse movimiento

--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -17,7 +17,7 @@ namespace TGC.MonoGame.TP
         public const string ContentFolderMusic = "Music/";
         public const string ContentFolderSounds = "Sounds/";
         public const string ContentFolderSpriteFonts = "SpriteFonts/";
-        public const string ContentFolderTextures = "Textures/";
+        public const string ContentFolderTextures = "Models/textures_mod/";
 
         /// <summary>
         ///     Constructor del juego.
@@ -39,12 +39,11 @@ namespace TGC.MonoGame.TP
 
         private GraphicsDeviceManager Graphics { get; }
         private SpriteBatch SpriteBatch { get; set; }
-        private Model Model { get; set; }
         private Effect Effect { get; set; }
-        private float Rotation { get; set; }
-        private Matrix World { get; set; }
         private Matrix View { get; set; }
         private Matrix Projection { get; set; }
+
+        private Tank tanque;
 
         /// <summary>
         ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
@@ -62,9 +61,7 @@ namespace TGC.MonoGame.TP
             GraphicsDevice.RasterizerState = rasterizerState;
             // Seria hasta aca.
 
-            // Configuramos nuestras matrices de la escena.
-            World = Matrix.Identity;
-            View = Matrix.CreateLookAt(Vector3.UnitZ * 150, Vector3.Zero, Vector3.Up);
+            View = Matrix.CreateLookAt(new Vector3(15,10,0), Vector3.Zero, Vector3.Up);
             Projection =
                 Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 250);
 
@@ -78,26 +75,17 @@ namespace TGC.MonoGame.TP
         /// </summary>
         protected override void LoadContent()
         {
+            
             // Aca es donde deberiamos cargar todos los contenido necesarios antes de iniciar el juego.
             SpriteBatch = new SpriteBatch(GraphicsDevice);
-
+            //var texture = Content.Load<Texture2D>(ContentFolderTextures + "hullA"); 
             // Cargo el modelo del logo.
-            Model = Content.Load<Model>(ContentFolder3D + "tgc-logo/tgc-logo");
+            Model model = Content.Load<Model>(ContentFolder3D + "T90");
 
             // Cargo un efecto basico propio declarado en el Content pipeline.
             // En el juego no pueden usar BasicEffect de MG, deben usar siempre efectos propios.
-            Effect = Content.Load<Effect>(ContentFolderEffects + "BasicShader");
-
-            // Asigno el efecto que cargue a cada parte del mesh.
-            // Un modelo puede tener mas de 1 mesh internamente.
-            foreach (var mesh in Model.Meshes)
-            {
-                // Un mesh puede tener mas de 1 mesh part (cada 1 puede tener su propio efecto).
-                foreach (var meshPart in mesh.MeshParts)
-                {
-                    meshPart.Effect = Effect;
-                }
-            }
+            Effect effect = Content.Load<Effect>(ContentFolderEffects + "BasicShader");
+            tanque = new Tank(model,effect);
 
             base.LoadContent();
         }
@@ -118,10 +106,7 @@ namespace TGC.MonoGame.TP
                 Exit();
             }
             
-            // Basado en el tiempo que paso se va generando una rotacion.
-            Rotation += Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
-
-            World = Matrix.CreateRotationY(Rotation);
+            tanque.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -132,24 +117,10 @@ namespace TGC.MonoGame.TP
         /// </summary>
         protected override void Draw(GameTime gameTime)
         {
-            // Aca deberiamos poner toda la logia de renderizado del juego.
             GraphicsDevice.Clear(Color.Black);
-
-            // Para dibujar le modelo necesitamos pasarle informacion que el efecto esta esperando.
-            Effect.Parameters["View"].SetValue(View);
-            Effect.Parameters["Projection"].SetValue(Projection);
-            Effect.Parameters["DiffuseColor"].SetValue(Color.DarkBlue.ToVector3());
-
-            foreach (var mesh in Model.Meshes)
-            {
-                Effect.Parameters["World"].SetValue(mesh.ParentBone.Transform * World);
-                mesh.Draw();
-            }
+            tanque.Draw(View,Projection);
         }
 
-        /// <summary>
-        ///     Libero los recursos que se cargaron en el juego.
-        /// </summary>
         protected override void UnloadContent()
         {
             // Libero los recursos.
---
+++ TGC.MonoGame.TP/Tank.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Net.Mime;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+public class Tank{
+    private Model Model { get; set; }
+    private Effect Effect { get; set; }
+    //private Texture2D tex { get; set; }
+    private Vector3 _rotation { get; set; }
+
+    private float yaw = 0;
+    private Vector3 _position;
+
+    private Matrix localRotation;
+    private Matrix World { get; set; }
+
+    public Tank(Model modelo,Effect efecto)
+    {
+        //Cree esta porque viene mirando para arriba el tanque.
+        localRotation = Matrix.Identity * Matrix.CreateRotationX(MathHelper.ToRadians(-90)) * Matrix.CreateRotationY(MathHelper.ToRadians(90));
+        _rotation = Vector3.Transform(Vector3.Up,localRotation);
+        Model = modelo;
+        Effect = efecto;
+        //tex = t;
+        foreach (var mesh in Model.Meshes)
+        {
+            // Un mesh puede tener mas de 1 mesh part (cada 1 puede tener su propio efecto).
+            foreach (var meshPart in mesh.MeshParts)
+            {
+                meshPart.Effect = Effect;
+            }
+        }
+        //Model.Meshes[0].MeshParts[0].Effect.Parameters["Texture"].SetValue(t); 
+}
+
+    /*protected override void LoadContent(String ontent, String effect)
+    {
+
+        // Cargo el modelo del logo.
+        Model = Content.Load<Model>(ContentFolder3D + "T90");
+
+        // Cargo un efecto basico propio declarado en el Content pipeline.
+        // En el juego no pueden usar BasicEffect de MG, deben usar siempre efectos propios.
+        Effect = Content.Load<Effect>(ContentFolderEffects + "BasicShader");
+
+        // Asigno el efecto que cargue a cada parte del mesh.
+        // Un modelo puede tener mas de 1 mesh internamente.
+        foreach (var mesh in Model.Meshes)
+        {
+            // Un mesh puede tener mas de 1 mesh part (cada 1 puede tener su propio efecto).
+            foreach (var meshPart in mesh.MeshParts)
+            {
+                meshPart.Effect = Effect;
+            }
+        }
+
+        base.LoadContent();
+    }*/
+
+
+    public void Update(GameTime gameTime)
+    {
+        
+
+        // Falta el elapsed time y velocidades
+        if (Keyboard.GetState().IsKeyDown(Keys.W))
+        {
+            _position += _rotation *0.5f;
+        }
+
+        if (Keyboard.GetState().IsKeyDown(Keys.S))
+        {
+            _position -= _rotation *0.5f;
+        }
+
+        if (Keyboard.GetState().IsKeyDown(Keys.A))
+        {
+            yaw += 0.1f;
+            _rotation = Vector3.Transform(_rotation,Matrix.CreateRotationY(0.1f));
+        }
+        if (Keyboard.GetState().IsKeyDown(Keys.D))
+        {
+            yaw -= 0.1f;
+            _rotation = Vector3.Transform(_rotation,Matrix.CreateRotationY(-0.1f));
+        }
+        
+        
+        World = localRotation * Matrix.CreateRotationY(yaw) * Matrix.CreateTranslation(_position);
+    }
+
+    public void Draw(Matrix View, Matrix Projection)
+    {
+        float i = 0.1f;
+
+        Effect.Parameters["View"].SetValue(View);
+        Effect.Parameters["Projection"].SetValue(Projection);
+
+        foreach (var mesh in Model.Meshes)
+        {
+            Effect.Parameters["World"].SetValue(mesh.ParentBone.Transform * World);
+            Effect.Parameters["DiffuseColor"].SetValue(new Vector3(i+0.1f,i,i-0.1f));
+            mesh.Draw();
+            i+=0.1f;
+            if(i>=1f)
+                i=0.1f;
+        }
+    }
+}
